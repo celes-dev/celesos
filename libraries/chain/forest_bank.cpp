99ed282e18c1316a9bc3be9aaf06196ec9d58e43
hubery.zhang <hubery.zhang@celeschain.io>
2018-08-13 13:45:26 +0800

    Integration verify code and rearranged logic

--- libraries/chain/forest_bank.cpp
+++ libraries/chain/forest_bank.cpp
@@ -28,96 +28,94 @@ namespace celesos{
         }
 
 
-        int get_index_of_signs(char ch) {
-            if(ch >= '0' && ch <= '9')
-            {
-                return ch - '0';
-            }
-            if(ch >= 'A' && ch <='F')
-            {
-                return ch - 'A' + 10;
-            }
-            if(ch >= 'a' && ch <= 'f')
-            {
-                return ch - 'a' + 10;
-            }
-            return -1;
-        }
-        boost::multiprecision::uint512_t hex_to_dec(const char *source) {
-            boost::multiprecision::uint512_t sum = 0;
-            boost::multiprecision::uint512_t t = 1;
-            size_t i;
-
-            size_t len = strlen(source);
-            for(i=len-1; i>=0; i--)
-            {
-                sum += t * get_index_of_signs(*(source + i));
-                t *= 16;
-            }
-
-            return sum;
-        }
+//        int get_index_of_signs(char ch) {
+//            if(ch >= '0' && ch <= '9')
+//            {
+//                return ch - '0';
+//            }
+//            if(ch >= 'A' && ch <='F')
+//            {
+//                return ch - 'A' + 10;
+//            }
+//            if(ch >= 'a' && ch <= 'f')
+//            {
+//                return ch - 'a' + 10;
+//            }
+//            return -1;
+//        }
+//        boost::multiprecision::uint512_t hex_to_dec(const char *source) {
+//            boost::multiprecision::uint512_t sum = 0;
+//            boost::multiprecision::uint512_t t = 1;
+//            size_t i;
+//
+//            size_t len = strlen(source);
+//            for(i=len-1; i>=0; i--)
+//            {
+//                sum += t * get_index_of_signs(*(source + i));
+//                t *= 16;
+//            }
+//
+//            return sum;
+//        }
         bool forest_bank::verify_wood(uint32_t block_number, const account_name& account, const uint64_t wood){
             const controller control = controller::controller(controller::config());
             uint32_t current_block_number = control.head_block_num();
             if(block_number <= current_block_number - question_period){
-                //过期了
+                //wood is past due
                 return false;
             } else if(block_number%question_space_number != 0){
-                //不是问题
+                //not forest
                 return false;
             }else{
-                //这里验证答案是否正确
+                //in here verify wood is validity
                 signed_block_ptr block_ptr = control.fetch_block_by_number(block_number);
-                //这里获取目标难度
-                boost::multiprecision::uint512_t target = 1<<58;
-                //准备需要验证的参数
+                //get forest target
+                boost::multiprecision::uint256_t target = 1<<58;
+                //prepare parameter for ethash
                 uint32_t cache_number = block_number/question_period;
+                std::vector<celesos::ethash::node> cache_data;
                 if(cache_number == first_cache_pair.first){
-                    string cache = first_cache_pair.second;
+                    cache_data = first_cache_pair.second;
+                }else if(cache_number == second_scahe_pair.first){
+                    cache_data = second_scahe_pair.second;
+                }else{
+                    //not found matched period
+                    return false;
                 }
 
                 block_id_type block_id = control.get_block_id_for_num(block_number);
                 block_id_type wood_forest = fc::sha256::hash(block_id.str()+account.to_string());
-                //调用验证函数
-                fc::sha256 result_hash = fc::sha256::hash("wood");
+                uint32_t data_set_count = 16777216;
+                //call ethash verify wood
+                uint256_t result_value = celesos::ethash::hash_light(wood_forest,wood,data_set_count,cache_data);
 
-                const char *result_hash_char = result_hash.str().c_str();
-                boost::multiprecision::uint256_t result_value = hex_to_dec(result_hash_char);
-
-                //update from meet
-                if(result_value <= target){
-                    //难度符合目标难度
-                    return true;
-
-                }else{
-                    //难度不符合目标难度
-                    return false;
-                }
+                return result_value <= target ? true : false;
             }
         }
 
         void forest_bank::update_cache(uint32_t block_number){
             //保留最近的两个cache
             uint32_t current_cache_number = block_number/question_period;
-            if(first_cache_pair.second == NULL)
+            const controller control = controller::controller(controller::config());
+
+            std::vector<celesos::ethash::node> node_vector;
+            uint32_t dataset_count = 16777216;
+            block_id_type seed = control.get_block_id_for_num(current_cache_number);
+            if(celesos::ethash::calc_cache(node_vector,dataset_count,seed.str())){
+                first_cache_pair = std::make_pair(current_cache_number, &node_vector);
+            }
+
+            if(!(first_cache_pair.second.empty()))
             {
-                first_cache_pair = std::make_pair(current_cache_number, "cache feed");
-            }else {
                 second_scahe_pair = first_cache_pair;
-                first_cache_pair = std::make_pair(current_cache_number, "cache feed");
             }
         }
 
         bool forest_bank::get_forest(forest_struct& forest, const account_name& account){
             const controller control = controller::controller(controller::config());
             uint32_t current_block_number = control.head_block_num();
-            //试图更新cache
-            update_cache(current_block_number);
 
-            // udpate
             uint32_t current_forest_number = current_block_number/question_space_number * question_space_number;
-
 
             if((forest_struct.target == 0)
                || current_forest_number != forest_struct.block_number){
@@ -134,7 +132,5 @@ namespace celesos{
             }
             return true;
         }
-
-
     }
 }
