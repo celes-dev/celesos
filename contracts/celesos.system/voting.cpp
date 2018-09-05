06fc7e3493e91dcb573dfaadd9af2d754320d3f4
dickyang <dickyang@celeschain.io>
2018-09-05 16:17:04 +0800

    b

--- contracts/celesos.system/voting.cpp
+++ contracts/celesos.system/voting.cpp
@@ -431,15 +431,15 @@ namespace eosiosystem {
 #if LOG_ENABLE
         eosio::print("diff.......2:");
 #endif
-        auto diff1 = (last1 == _burnblockstatinfos.end()) ? 1 : last1->diff;
-        auto wood1 = (last1 == _burnblockstatinfos.end()) ? target_wood_number : last1->stat;
-        auto diff2 = (last2 == _burnblockstatinfos.end()) ? 1 : last2->diff;
-        auto wood2 = (last2 == _burnblockstatinfos.end()) ? target_wood_number : last2->stat;
-        auto diff3 = (last3 == _burnblockstatinfos.end()) ? 1 : last3->diff;
-        auto wood3 = (last3 == _burnblockstatinfos.end()) ? target_wood_number : last3->stat;
-#if LOG_ENABLE
-        eosio::print("diff.......3:");
-#endif
+//        auto diff1 = (last1 == _burnblockstatinfos.end()) ? 1 : last1->diff;
+//        auto wood1 = (last1 == _burnblockstatinfos.end()) ? target_wood_number : last1->stat;
+//        auto diff2 = (last2 == _burnblockstatinfos.end()) ? 1 : last2->diff;
+//        auto wood2 = (last2 == _burnblockstatinfos.end()) ? target_wood_number : last2->stat;
+//        auto diff3 = (last3 == _burnblockstatinfos.end()) ? 1 : last3->diff;
+//        auto wood3 = (last3 == _burnblockstatinfos.end()) ? target_wood_number : last3->stat;
+//#if LOG_ENABLE
+//        eosio::print("diff.......3:");
+//#endif
 //        // Suppose the last 3 cycle,the diff is diff1,diff2,diff2, and the answers count is wood1,wood2,wood3
 //        // 假设历史三个周期难度分别为diff1,diff2,diff3,对应提交的答案数为wood1,wood2,wood3(1为距离当前时间最短的周期)
 //        // so suggest diff is:M/wood1*diff1*1/7+M/wood2*diif2*2/7+M/wood3*diff3*4/7,Simplified to M/7*(diff1/wood1+2*diif2/wood2+4*diff3/wood3)
