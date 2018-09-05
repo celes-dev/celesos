0ce49b950887e499a50b686a21e4261d4187aa4a
dickyang <dickyang@celeschain.io>
2018-09-05 16:10:47 +0800

    a

--- contracts/celesos.system/voting.cpp
+++ contracts/celesos.system/voting.cpp
@@ -440,29 +440,29 @@ namespace eosiosystem {
 #if LOG_ENABLE
         eosio::print("diff.......3:");
 #endif
-        // Suppose the last 3 cycle,the diff is diff1,diff2,diff2, and the answers count is wood1,wood2,wood3
-        // 假设历史三个周期难度分别为diff1,diff2,diff3,对应提交的答案数为wood1,wood2,wood3(1为距离当前时间最短的周期)
-        // so suggest diff is:M/wood1*diff1*1/7+M/wood2*diif2*2/7+M/wood3*diff3*4/7,Simplified to M/7*(diff1/wood1+2*diif2/wood2+4*diff3/wood3)
-        // 则建议难度值为M/wood1*diff1*1/7+M/wood2*diif2*2/7+M/wood3*diff3*4/7,简化为M/7*(diff1/wood1+2*diif2/wood2+4*diff3/wood3)
-        auto targetdiff = ((double) target_wood_number) / 7 * (wood1 / diff1 * 4 + wood2 / diff2 * 2 + wood3 / diff3);
-#if LOG_ENABLE
-        eosio::print("diff.......4:");
-#endif
-        auto current = _burnblockstatinfos.find(block_number);
-        if (current == _burnblockstatinfos.end()) {
-            // payer is the system account
-            _burnblockstatinfos.emplace(N(eosio), [&](auto &p) {
-                p.block_number = block_number;
-                p.diff = targetdiff;
-            });
-        } else {
-            _burnblockstatinfos.modify(current, 0, [&](auto &p) {
-                p.diff = targetdiff;
-            });
-        }
-#if LOG_ENABLE
-        eosio::print("diff.......5:");
-#endif
+//        // Suppose the last 3 cycle,the diff is diff1,diff2,diff2, and the answers count is wood1,wood2,wood3
+//        // 假设历史三个周期难度分别为diff1,diff2,diff3,对应提交的答案数为wood1,wood2,wood3(1为距离当前时间最短的周期)
+//        // so suggest diff is:M/wood1*diff1*1/7+M/wood2*diif2*2/7+M/wood3*diff3*4/7,Simplified to M/7*(diff1/wood1+2*diif2/wood2+4*diff3/wood3)
+//        // 则建议难度值为M/wood1*diff1*1/7+M/wood2*diif2*2/7+M/wood3*diff3*4/7,简化为M/7*(diff1/wood1+2*diif2/wood2+4*diff3/wood3)
+//        auto targetdiff = ((double) target_wood_number) / 7 * (wood1 / diff1 * 4 + wood2 / diff2 * 2 + wood3 / diff3);
+//#if LOG_ENABLE
+//        eosio::print("diff.......4:");
+//#endif
+//        auto current = _burnblockstatinfos.find(block_number);
+//        if (current == _burnblockstatinfos.end()) {
+//            // payer is the system account
+//            _burnblockstatinfos.emplace(N(eosio), [&](auto &p) {
+//                p.block_number = block_number;
+//                p.diff = targetdiff;
+//            });
+//        } else {
+//            _burnblockstatinfos.modify(current, 0, [&](auto &p) {
+//                p.diff = targetdiff;
+//            });
+//        }
+//#if LOG_ENABLE
+//        eosio::print("diff.......5:");
+//#endif
         return targetdiff;
     }
 
