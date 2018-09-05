3f5abe846cd48ffb50ca0779d396b671a13bf753
dickyang <dickyang@celeschain.io>
2018-09-05 17:33:08 +0800

    aaa'

--- contracts/celesos.system/voting.cpp
+++ contracts/celesos.system/voting.cpp
@@ -448,11 +448,14 @@ namespace eosiosystem {
 #endif
         auto current = _burnblockstatinfos.find(block_number);
         if (current == _burnblockstatinfos.end()) {
-            // payer is the system account
-            _burnblockstatinfos.emplace(N(eosio), [&](auto &p) {
-                p.block_number = block_number;
-                p.diff = targetdiff;
-            });
+#if LOG_ENABLE
+            eosio::print("insert:",targetdiff,"\r\n");
+#endif
+//            // payer is the system account
+//            _burnblockstatinfos.emplace(N(eosio), [&](auto &p) {
+//                p.block_number = block_number;
+//                p.diff = targetdiff;
+//            });
         }
 //        else {
 //            _burnblockstatinfos.modify(current, 0, [&](auto &p) {
