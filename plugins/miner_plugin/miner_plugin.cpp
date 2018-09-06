9cef99c893cdeb2285448bb1250683ff28ddea58
dickyang <dickyang@celeschain.io>
2018-09-06 10:38:22 +0800

    change log

--- plugins/miner_plugin/miner_plugin.cpp
+++ plugins/miner_plugin/miner_plugin.cpp
@@ -196,7 +196,7 @@ void celesos::miner_plugin::plugin_startup() {
                         std::string wood_hex{130, char{}, std::string::allocator_type{}};
                         ethash::uint256_to_hex(wood_hex, wood_opt.get());
 
-                        ilog("wood...wood...wood:{wood}",("wood",wood_opt.get()));
+                        ilog("wood...wood...wood:${wood}",("wood",wood_opt.get()));
 
                         chain::signed_transaction tx{};
                         vector<chain::permission_level> auth{{voter_name, "active"}};
