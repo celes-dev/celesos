c10e3a96d9307a63d51783e023e0a67a60bd2e3e
dickyang <dickyang@celeschain.io>
2018-09-14 18:02:58 +0800

    test by cutide

--- libraries/chain/include/eosio/chain/block_header.hpp
+++ libraries/chain/include/eosio/chain/block_header.hpp
@@ -39,7 +39,7 @@ namespace eosio { namespace chain {
       /**
        * The difficulity value for pow
        */
-      optional<double>                  difficulty;
+      double                  difficulty;
       /// @}
 
       digest_type       digest()const;
