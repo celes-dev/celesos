d3465226ad3b650649664e37cd9304e21f1494d7
dickyang <dickyang@celeschain.io>
2018-09-27 10:25:15 +0800

    delete some log

--- plugins/miner_plugin/miner_plugin.cpp
+++ plugins/miner_plugin/miner_plugin.cpp
@@ -216,8 +216,8 @@ void celesos::miner_plugin::plugin_startup() {
                 [this, &the_chain_plugin, launch_time](auto is_success,
                                                        auto block_num,
                                                        const auto &wood_opt) {
-                    ilog("Receive mine callback with is_success: ${is_success} block_num: ${block_num}",
-                         ("is_success", is_success)("block_num", block_num));
+//                    ilog("Receive mine callback with is_success: ${is_success} block_num: ${block_num}",
+//                         ("is_success", is_success)("block_num", block_num));
 
                     //TODO 考虑系统合约未安装的情况
                     if (!is_success) {
@@ -231,7 +231,7 @@ void celesos::miner_plugin::plugin_startup() {
                     }
 
                     try {
-                        ilog("begin prepare transaction about voteproducer");
+//                        ilog("begin prepare transaction about voteproducer");
                         auto &cc = the_chain_plugin.chain();
                         const auto &chain_id = cc.get_chain_id();
                         const auto &voter_name = this->my->_voter_name;
@@ -240,8 +240,8 @@ void celesos::miner_plugin::plugin_startup() {
                         std::string wood_hex{};
                         ethash::uint256_to_hex(wood_hex, wood_opt.get());
                         auto bank = forest::forest_bank::getInstance(the_chain_plugin.chain());
-                        ilog("wood should pass varify check: ${is_pass}",
-                             ("is_pass", bank->verify_wood(block_num, voter_name, wood_hex.c_str())));
+//                        ilog("wood should pass varify check: ${is_pass}",
+//                             ("is_pass", bank->verify_wood(block_num, voter_name, wood_hex.c_str())));
 
                         chain::signed_transaction tx{};
                         vector<chain::permission_level> auth{{voter_name, "active"}};
@@ -268,18 +268,18 @@ void celesos::miner_plugin::plugin_startup() {
                             tx.signatures.push_back(signature);
                         }
                         auto packed_tx_ptr = std::make_shared<chain::packed_transaction>(chain::packed_transaction{tx});
-                        ilog("end prepare transaction about voteproducer");
+//                        ilog("end prepare transaction about voteproducer");
                         using method_type = chain::plugin_interface::incoming::methods::transaction_async;
                         using handler_param_type = fc::static_variant<fc::exception_ptr, chain::transaction_trace_ptr>;
-                        ilog("start to push transation about voteproducer");
+//                        ilog("start to push transation about voteproducer");
                         auto handler = [](const handler_param_type &param) {
                             if (param.contains<fc::exception_ptr>()) {
                                 auto exp_ptr = param.get<fc::exception_ptr>();
-                                ilog("fail to push transaction about voteproducer with error: \n${error}",
-                                     ("error", exp_ptr->to_detail_string().c_str()));
+//                                ilog("fail to push transaction about voteproducer with error: \n${error}",
+//                                     ("error", exp_ptr->to_detail_string().c_str()));
                             } else {
                                 auto trace_ptr = param.get<chain::transaction_trace_ptr>();
-                                ilog("suceess to push transaction about voteproducer");
+//                                ilog("suceess to push transaction about voteproducer");
                             }
                         };
                         app().get_method<method_type>()(packed_tx_ptr, true, handler);
