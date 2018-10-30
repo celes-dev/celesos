9429a51b1cdbc951b1ff0247bee034e2e694fad8
yale.zheng <yale.zheng@celeschain.io>
2018-10-30 16:29:45 +0800

    fix script error on docker image

--- Docker-Celesos/clceles.sh
+++ Docker-Celesos/clceles.sh
@@ -2,13 +2,13 @@
 
 # Usage:
 # Go into cmd loop: sudo ./clceles.sh
-# Run single cmd:  sudo ./clceles.sh <cleos paramers>
+# Run single cmd:  sudo ./clceles.sh <clceles paramers>
 
-PREFIX="docker-compose exec nodeosd cleos"
+PREFIX="docker-compose exec nodcelesd clceles"
 if [ -z $1 ] ; then
   while :
   do
-    read -e -p "cleos " cmd
+    read -e -p "clceles " cmd
     history -s "$cmd"
     $PREFIX $cmd
   done
