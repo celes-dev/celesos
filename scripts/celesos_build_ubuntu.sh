90aec017854e3caa72f338ceb34891e18b7292bd
yale.zheng <yale.zheng@celeschain.io>
2018-11-09 15:28:34 +0800

    sync scripts from eos and remove useless scripts

--- scripts/celesos_build_ubuntu.sh
+++ scripts/celesos_build_ubuntu.sh
@@ -27,7 +27,7 @@
 	if [ "${MEM_MEG}" -lt 7000 ]; then
 		printf "\\tYour system must have 7 or more Gigabytes of physical memory installed.\\n"
 		printf "\\tExiting now.\\n"
-		#exit 1
+		exit 1
 	fi
 
 	case "${OS_NAME}" in
