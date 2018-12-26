7405b9b7db079a27d599b5a61ceaf213eea32e75
hubery.zhang <hubery.zhang@celeschain.io>
2018-12-26 13:19:19 +0800

    debug

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1535,7 +1535,7 @@ struct controller_impl {
          signed_block_ptr blk_state = self.fetch_block_by_number( block_number );
          if(blk_state == nullptr){
             ilog("set_my_random block_number:${block_number} block is nullptr",("block_number",block_number));
-            return;
+            continue;
          }
          ilog("p producer:${producer}",("producer",p->header.producer));
          ilog("blk_state producer:${producer}",("producer",blk_state->producer));
